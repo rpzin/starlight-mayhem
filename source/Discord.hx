c84b7140195b48e971f4881352f3bb57c4d255dc
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-08 00:54:55 +0200

    No discord

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,7 +1,9 @@
 package;
 
+#if desktop
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
+#end
 
 #if LUA_ALLOWED
 import llua.Lua;
@@ -14,6 +16,7 @@ class DiscordClient
 {
 	public function new()
 	{
+                #if desktop
 		trace("Discord Client starting...");
 		DiscordRpc.start({
 			clientID: "863222024192262205",
@@ -31,21 +34,26 @@ class DiscordClient
 		}
 
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	public static function shutdown()
 	{
+                #if desktop
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	static function onReady()
 	{
+                #if desktop
 		DiscordRpc.presence({
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
 			largeImageText: "Starlight Mayhem"
 		});
+                #end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -60,15 +68,18 @@ class DiscordClient
 
 	public static function initialize()
 	{
+                #if desktop
 		var DiscordDaemon = sys.thread.Thread.create(() ->
 		{
 			new DiscordClient();
 		});
 		trace("Discord Client initialized");
+                #end
 	}
 
 	public static function changePresence(details:String, state:Null<String>, ?smallImageKey : String, ?hasStartTimestamp : Bool, ?endTimestamp: Float)
 	{
+                #if desktop
 		var startTimestamp:Float = if(hasStartTimestamp) Date.now().getTime() else 0;
 
 		if (endTimestamp > 0)
@@ -84,8 +95,9 @@ class DiscordClient
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
-            endTimestamp : Std.int(endTimestamp / 1000)
+                        endTimestamp : Std.int(endTimestamp / 1000)
 		});
+                #end
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
